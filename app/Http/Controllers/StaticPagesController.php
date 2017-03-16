114795530e3c4cf4a93e0231fed660fb489af981
Shirley <shirley@gmail.com>
2017-03-16 07:49:52 +0000

    Finish static pages

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class StaticPagesController extends Controller
+{
+    // 主页
+    public function home() {
+    	return view('static_pages/home');
+    }
+
+    // 帮助页
+    public function help() {
+    	return view('static_pages/help');
+    }
+    // 关于页
+    public function about() {
+    	return view('static_pages/about');
+    }
+}
